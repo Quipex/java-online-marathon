d3ba8d9cde381f3d4da3e02912bf29a42643e253
Quipex <rudas72@gmail.com>
2020-04-06 01:06:31 +0300

    Added text of the problems

--- src/main/java/sprint02/MySqrt.java
+++ src/main/java/sprint02/MySqrt.java
@@ -1,5 +1,23 @@
 package sprint02;
 
+/*
+https://leetcode.com/problems/sqrtx/
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+Input: 4
+Output: 2
+
+Example 2:
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since
+             the decimal part is truncated, 2 is returned.
+ */
 public class MySqrt {
     public static int mySqrt(int x) {
 //        edge case
